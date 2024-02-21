938c415a8acb2eeda20b83c5f364cf3516f85ebd
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2024-02-22 11:24:37 +1300

    Working statement generator

---
+++ C_01_Statement_Generator.py
@@ -0,0 +1,9 @@
+# Functions go here
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+
+# Main routine goes here
+statement_generator("Instructions", "*")
+
+
