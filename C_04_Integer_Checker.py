27bc98a596d3a24f4a7a6a24729602e1413b2b2f
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2024-02-27 14:27:10 +1300

    Integer checker - issue with low print statement

---
+++ C_04_Integer_Checker.py
@@ -0,0 +1,33 @@
+# Ask user for width and loop until they
+# enter a number that is more than zero
+def int_check(question, low):
+
+    error = "Please enter a number that is more than or equal to {low}\n"
+    while True:
+        try:
+            # Ask the user for a number
+            response = int(input(question))
+
+            # Check that the number is more than zero
+            if response >= low:
+                return response
+            else:
+                print(error)
+        except ValueError:
+            print(error)
+
+
+# Main routine goes here
+for item in range(0, 1):
+    integer = int_check("Integer: ", 0)
+    print(integer)
+
+for item in range(0, 1):
+    width = int_check("Width: ", 1)
+    print(width)
+
+print()
+
+for item in range(0, 1):
+    height = int_check("Height: ", 1)
+    print(height)
